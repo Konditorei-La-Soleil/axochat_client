c439a62a4da3ff4fdbb9f3ed914550e7b98fdc12
木葉 Scarlet <93977077+mukjepscarlet@users.noreply.github.com>
2025-04-12 19:30:05 +0800

    Maven publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     kotlin("jvm") version "2.1.20"
+    `maven-publish`
 }
+
+group = "io.github.kls"
+version = "0.1.0"
 
 allprojects {
     repositories {
@@ -10,11 +14,31 @@ allprojects {
 
 subprojects {
     apply(plugin = "org.jetbrains.kotlin.jvm")
+    apply(plugin = "maven-publish")
+
+    group = rootProject.group
+    version = rootProject.version
 
     kotlin {
         jvmToolchain(8)
     }
-}
 
-group = "io.github.kls"
-version = "0.1.0"
+    tasks.jar {
+        archiveBaseName.set(this@subprojects.name)
+        manifest {
+            attributes["Implementation-Title"] = this@subprojects.name
+            attributes["Implementation-Version"] = version
+        }
+    }
+
+    publishing {
+        publications {
+            create<MavenPublication>("mavenJava") {
+                from(components["java"])
+            }
+        }
+        repositories {
+            mavenLocal()
+        }
+    }
+}
